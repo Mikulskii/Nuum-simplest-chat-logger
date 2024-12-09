bb90313a4a48adf6c0da934f193dc946c14278dd
Mikulski_ <vsevolod.mikulsky@yandex.com>
2024-12-09 11:43:16 +0700

    Update nuum_chat_logger.js

--- nuum_chat_logger.js
+++ nuum_chat_logger.js
@@ -2,7 +2,7 @@
 //https://nuum.ru/channel/<идентификатор>
 const channel = '';
 
-console.log("NUUM чат-логгер стартовал..") 
+console.log("NUUM чат-логгер стартовал.."); 
 
 //Получение Id-стрима
 function grab_id() {
@@ -10,7 +10,7 @@ fetch(`https://nuum.ru/api/v2/broadcasts/public?with_extra=true&channel_name=${c
 .then(response=> {
   if (!response.ok) {
 console.log(`При получении Id-стрима что-то пошло не так: ${response.status} (${response.statusText})`);
-process.exit(1)
+process.exit(1);
   }
 return response.json();
   })
@@ -19,10 +19,10 @@ return response.json();
 //Если стрим оффлайн, отмена
   if (data.result.channel.channel_is_live === false)
   {
-return console.log(`Stream is Offline`)
+return console.log(`Stream is Offline`);
   }
 let stream_id = data.result.media_container.media_container_id;
-console.log(`Получен Id-стрима: ${stream_id}`)
+console.log(`Получен Id-стрима: ${stream_id}`);
 
 //Получение Id-чата
 function grab_chat_id() {
@@ -30,14 +30,14 @@ fetch(`https://nuum.ru/api/v3/chats?contentType=SINGLE&contentId=${stream_id}`)
 .then(response=> {
   if (!response.ok) {
 console.log(`При получении Id-чата что-то пошло не так: ${response.status} (${response.statusText})`);
-process.exit(1)
+process.exit(1);
   }
 return response.json();
   })
 .then(data => {
   let chat_id = data.result?.id;
-  console.log(`Получен Id-чата: ${chat_id}`)
-  console.log(`Подключение к чату ${channel}..`)
+  console.log(`Получен Id-чата: ${chat_id}`);
+  console.log(`Подключение к чату ${channel}..`);
 
 //Получение сообщений из чата
 function nuum() {
@@ -62,13 +62,13 @@ return response.json();
 if (data?.result[0] === undefined) {return}
 
 //Назначение переменных на пришедший результат  
-  const nickname = data.result[0]?.author.login
-  const msg = data.result[0]?.eventData.text
-  const prev_msg = data.result[1]?.eventData.text
+  const nickname = data.result[0]?.author.login;
+  const msg = data.result[0]?.eventData.text;
+  const prev_msg = data.result[1]?.eventData.text;
 
 //Временные отметки для дальнейшей фильтрации
-  var  unix = Date.now()
-  const time_posted = data.result[0]?.timestamp
+  var  unix = Date.now();
+  const time_posted = data.result[0]?.timestamp;
 
 //Фильтр сообщений по временным отметкам 
   if (new Date(time_posted).getTime() < unix - 10150) {return}
@@ -76,18 +76,18 @@ if (data?.result[0] === undefined) {return}
   if (msg === prev_msg) {return}
 
 //Вывод в лог сообщений "никнейм - текст сообщения"
-  console.log(`[NUUM | ${nickname}] ${msg}`)
+  console.log(`[NUUM | ${nickname}] ${msg}`);
 })
 .catch((error) => {
-return  console.log(error)
+return  console.log(error);
 });
 }
-nuum()
+nuum();
 //Интервал запросов в мс
-setInterval(nuum, 10000)
-}) 
+setInterval(nuum, 10000);
+});
 }  
-grab_chat_id()
-})
+grab_chat_id();
+});
 }
-grab_id()
+grab_id();
